296b9e5c7db3778d48e5db98a9ed8d3f8dc261bd
CarlosDev <c-gvc@hotmail.com>
2025-02-12 03:37:03 +0100

    commit 03 implemetacion de WTForm

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from forms import SignupForm, PostForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
 '''@app.route("/")
 def holla():
@@ -10,26 +12,48 @@ posts = []
 
 @app.route("/")
 def index():
+    page = request.args.get('page', 1)
+    list = request.args.get('list', 20)
     '''return "{} posts".format(len(posts))'''
-    return render_template("index.html" , nun_posts=len(posts))
+    return render_template("index.html",  posts=posts)
 
 @app.route("/p/<string:slug>/")
 def show_post(slug):
     '''return "Mostrando el post {}".format(slug)'''
     return render_template("post_view.html" , slug_title=slug)
 
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>")
-def post_form(post_id=None):
-    '''return "post_form {}".format(post_id)'''
-    return render_template("admin/post_form.html" , post_id= post_id)
 
-'''print(url_for("index"))'''
-'''print(url_for("show_post", slug="leccion-1", preview=True))
-'''
-@app.route("/suma/<int:s1>/<int:s2>" , methods=["GET"])
-def suma(s1 , s2):
-    return (s1 + s2)
+@app.route("/admin/post/", methods=['GET' , 'POST'] , defaults={'post_id': None})
+@app.route("/admin/post/<int:post_id>" , methods=['GET' , 'POST'])
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
+
+        post = {'title': title , 'title_slug': title_slug , 'content': content}
+        posts.append(post)
+
+        return redirect(url_for("index"))
+    return render_template("admin/post_form.html" , form=form)
+
+
+@app.route("/signup/" , methods=["GET" , "POST"])
+def show_signup_form():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        next = request.args.get('next' , None)
+        if next:
+            return redirect(next)
+        return redirect(url_for("index"))
+    return render_template("signup_form.html" , form=form)
+
+
 
 if (__name__ == "__main__"):
     app.run(debug=True)
